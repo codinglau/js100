2bd1b1c2022b7bfec26e32aa9e25871a627948ea
codinglau <87560090+codinglau@users.noreply.github.com>
2021-07-21 21:08:20 +0800

    completed challenge 3

---
+++ 03-randomquotes/app.js
@@ -0,0 +1,46 @@
+const container = document.querySelector('.container');
+const btn = document.querySelector('button');
+const quote = document.querySelector('p');
+const author = document.querySelector('span');
+
+const quotes = [
+    {
+      quote:
+        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+      author: " Life"
+    },
+    {
+      quote:
+        "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+      author: "John Kenneth Galbraith"
+    },
+    {
+      quote:
+        "God save me from my friends. I can protect myself from my enemies.",
+      author: "Claude Louis Hector de Villars "
+    },
+    {
+      quote: "The price of anything is the amount of life you exchange for it.",
+      author: "David Thoreau"
+    },
+    {
+      quote:
+        "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+      author: "Charles Lindbergh"
+    },
+    {
+      quote:
+        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+      author: " Tyne Daly"
+    }
+];
+
+currentIndex = 1;
+quote.textContent = quotes[currentIndex].quote;
+author.textContent = quotes[currentIndex].author;
+
+btn.addEventListener('click', function(){
+    currentIndex < quotes.length - 1 ? currentIndex++ : currentIndex = 1;
+    quote.textContent = quotes[currentIndex].quote;
+    author.textContent = quotes[currentIndex].author;
+})
